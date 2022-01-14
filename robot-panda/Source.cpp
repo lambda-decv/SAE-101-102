e610bd8f122b8eabf8c451ca695ff7cedc97e760
Cortotrobo <corto.bouviolle@laposte.net>
2022-01-14 16:59:54 +0100

    ajout fonction init bambous

--- robot-panda/Source.cpp
+++ robot-panda/Source.cpp
@@ -6,9 +6,27 @@
 // Inclusion des headers
 #include "structures.h"
 
+
+
+
 using namespace std;
 
 
+const int TAILLE = 8;
+bambou bambous[TAILLE];
+
+
+void init_bambous(bambou tab[], int taille) {
+	for (int i = 0; i < taille; i++) {
+		tab[i].taille = 0;
+		tab[i].rang = i + 1;
+	}
+}
+
+
+
+
 int main(int argc, char* argv[]) {
-	
+	init_bambous(bambous, TAILLE);
+
 }
