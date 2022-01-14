b99f2b084d25be68c51e72fb693bbc9acc8c5c7f
Alexis BOIZARD <alexis.boizard@etu.univ-lyon1.fr>
2022-01-14 17:12:07 +0100

    ajout fonction lecture de fichier

--- robot-panda/Source.cpp
+++ robot-panda/Source.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<SDL.h>
 #include <SDL_ttf.h>
+#include <fstream>
+#include <string> 
 #include "config_sdl.h"
 
 // Inclusion des headers
@@ -8,7 +10,24 @@
 
 using namespace std;
 
+int lectFichier(const char nomFichier[10],int position) {
+    ifstream entree(nomFichier, ios::in);
+    char ligne[10];
+    char cpt = 0;
+    if (!entree)
+        cout << "Probleme d'ouverture " << endl;
+    else {
+        for (int i = 0; i < position; i++) {
+            entree.getline(ligne, 10);
+            cpt++;
+        }
+        entree.close();
+    }
+   
+    return atoi(ligne);
+}
 
 int main(int argc, char* argv[]) {
-	
+    
+    return 0;
 }
