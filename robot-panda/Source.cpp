2ab48c4a40add90024a646f4a25b8c59b1905011
Cortotrobo <corto.bouviolle@laposte.net>
2022-01-17 10:45:56 +0100

    hhhg

--- robot-panda/Source.cpp
+++ robot-panda/Source.cpp
@@ -34,8 +34,22 @@ int main(int argc, char* argv[]) {
             return EXIT_FAILURE;
         }
 
-        SDL_Window* pWindow{ nullptr };
-        SDL_Renderer* pRenderer{ nullptr };
+        //on crée la fenêtre
+
+        SDL_Window* pWindow = SDL_CreateWindow("Ma fenetre",
+            SDL_WINDOWPOS_CENTERED,     //pos. X: autre option: SDL_WINDOWPOS_UNDEFINED
+            SDL_WINDOWPOS_CENTERED,     //pos. Y: autre option: SDL_WINDOWPOS_UNDEFINED 
+            LARGEUR, 			//largeur en pixels			
+            HAUTEUR, 			//hauteur en pixels
+            SDL_WINDOW_SHOWN //d’autres options (plein ecran, resizable, sans bordure...)
+        );
+        if (win == NULL)
+            cout << "erreur ouverture fenetre";
+
+        SDL_Renderer* pRenderer= SDL_CreateRenderer(
+            win,  //nom de la fenêtre
+            -1, //par défaut
+            SDL_RENDERER_ACCELERATED); //utilisation du GPU, valeur recommandée
 
         if (SDL_CreateWindowAndRenderer(LARGEUR, HAUTEUR, SDL_WINDOW_SHOWN, &pWindow, &pRenderer) < 0)
         {
@@ -49,14 +63,20 @@ int main(int argc, char* argv[]) {
 
         SDL_Surface* image = IMG_Load("sol.png");
         SDL_Texture* pTextureImage = SDL_CreateTextureFromSurface(pRenderer, image);
+        SDL_Surface* image2 = IMG_Load("soleil.png");
+        SDL_Texture* pTextureImage2 = SDL_CreateTextureFromSurface(pRenderer, image2);
 
         SDL_FreeSurface(image);
         SDL_Rect src1{ 0, 0, 0, 0 };
-        SDL_Rect src2{ 0, 0, 0, 0 };
 
         SDL_Rect dst1{ 0, 380, 800, 100 };
+        
+        SDL_FreeSurface(image2);
+        SDL_Rect src2{ 0, 0, 0, 0 };
+        SDL_Rect dst2{ 700, 30, 50, 50 };
 
         SDL_QueryTexture(pTextureImage, nullptr, nullptr, &src1.w, &src1.h);
+        SDL_QueryTexture(pTextureImage2, nullptr, nullptr, &src2.w, &src2.h);
 
         // Récuperer les bons coordonnées x et y pour afficher la tête du tigre
         src2.x = src1.w / 2 - 30;
@@ -82,6 +102,7 @@ int main(int argc, char* argv[]) {
             SDL_RenderClear(pRenderer);
 
             SDL_RenderCopy(pRenderer, pTextureImage, &src1, &dst1); // Affiche la texture entièrement
+            SDL_RenderCopy(pRenderer, pTextureImage2, &src2, &dst2);
 
             SDL_RenderPresent(pRenderer);
         }
