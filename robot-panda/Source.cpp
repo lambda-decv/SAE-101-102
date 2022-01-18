081b18b861a22504458c6fd10feb971ee34acb0e
lambda_decv <bzdalexis@gmail.com>
2022-01-18 10:13:51 +0100

    fix tout

--- robot-panda/Source.cpp
+++ robot-panda/Source.cpp
@@ -71,31 +71,8 @@ int main(int argc, char* argv[]) {
 	SDL_Texture* pTextureRobot = SDL_CreateTextureFromSurface(rendu, robot);
 	SDL_FreeSurface(robot);
 
-	SDL_Rect rectangle{ 800, 0, 400, 480 };
-
-
-
-	
-
-	SDL_SetRenderDrawColor(rendu, 0, 242, 255, 255);
-	SDL_RenderClear(rendu);
-
-	SDL_RenderCopy(rendu, pTextureImage, &src1, &dst1); // Affiche la texture entièrement
-	SDL_RenderCopy(rendu, pTextureImage2, &src2, &dst2); // Affiche la texture entièrement
-
-	affichageRobot(rendu, robot, pTextureRobot, robotCo);
-	affichageRobot(rendu, robot, pTextureRobot, robotCo); // Affiche la texture entièrement
-	SDL_RenderCopy(rendu, pTextureImage2, &src2, &dst2); // Affiche la texture entièrement
-
-	SDL_SetRenderDrawColor(rendu, 255, 255, 255, 255);
-	SDL_RenderFillRect(rendu, &rectangle);
-
-
-	graph1(rendu);
-
-  affichageBg(rendu,pTextureImage,pTextureImage2);
+	affichageBg(rendu,pTextureImage,pTextureImage2);
 	affichageRobot(rendu, robotCo,pTextureRobot);
-
 
 	cycleJournalier(rendu, bambous,10, co,pTextureImage, pTextureImage2,pTextureRobot);
 
